797887224a5c1ebc73d513b2fb58d30fa4e97f3d
Play Epik Incorporation <help.playepik@gmail.com>
2025-08-28 16:34:45 +0200

    UPGRADE: Upgrade of EAC
    * upgraded to latest version available

--- Presets/Install_EasyAntiCheat.bat
+++ Presets/Install_EasyAntiCheat.bat
@@ -2,7 +2,7 @@
 
 NET SESSION >nul 2>&1
 if %errorLevel% == 0 (
-    "%~dp0\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe" install ffd2a4e23f75a7 REM substitute this number with the current activated-build on dev.epicgames.com
+    "%~dp0\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe" install a427befc43a069 REM substitute this number with the current activated-build on dev.epicgames.com
 ) else (
     echo This script requires administrator privileges.
     echo Please run it as an administrator.
