86758d9e77b77b802ea8e248970561ca173efa26
Play Epik Incorporation <help.playepik@gmail.com>
2025-08-28 16:40:30 +0200

    FIX: Fixing company
    * fixed company name

--- Configurator/Properties/AssemblyInfo.cs
+++ Configurator/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Configurator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Play Epik, Incorporation")]
 [assembly: AssemblyProduct("Configurator")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
