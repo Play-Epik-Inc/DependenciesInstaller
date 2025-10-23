25ec34666eaa32c6f4949ce65e312cffe2bea7ae
Play-Epik-Inc <help.playepik@gmail.com>
2025-10-23 18:51:57 +0200

    fix: removed some unusued calls

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,18 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DependenciesInstaller")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Play Epik, Incorporation")]
 [assembly: AssemblyProduct("DependenciesInstaller")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 
 [assembly: Guid("bef407e2-6cda-416b-a7e1-10b20ed814ba")]
 
-[assembly: AssemblyVersion("2.0.1.3")]
-[assembly: AssemblyFileVersion("2.0.1.3")]
+[assembly: AssemblyVersion("2.0.1.4")]
+[assembly: AssemblyFileVersion("2.0.1.4")]
